de38b918648a01f4586d7cec198592efdb1bb284
Aladinebn <ala.benna@gmail.com>
2026-04-05 18:23:30 +0100

    Add time for execution = 2 sec

--- src/test/java/base/BasicTest.java
+++ src/test/java/base/BasicTest.java
@@ -20,7 +20,7 @@ public class BasicTest {
     static void launchBrowser() {
         playwright = Playwright.create();
         browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(false)
+                new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(2000)
         );
     }
 
